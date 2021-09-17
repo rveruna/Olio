3ba617bc128714c289dd001514188d9256010a6b
Veronika Rosicova <verunar@seznam.cz>
2021-09-17 18:51:34 +0100

    displaying data in simple table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+//components
+import SimpleTable from './components/SimpleTable';
+
+// styles
 import './App.css';
 
+const ARTICLES_URL =
+  'https://s3-eu-west-1.amazonaws.com/olio-staging-images/developer/test-articles-v4.json';
+
 function App() {
+  const [data, setData] = useState({ articles: [], isFetching: false });
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        setData({ articles: data.articles, isFetching: true });
+        const response = await axios.get(ARTICLES_URL);
+        setData({ articles: response.data, isFetching: false });
+      } catch (e) {
+        console.log(e);
+        setData({ articles: data.articles, isFetching: false });
+      }
+    };
+    fetchArticles();
+  }, []);
   return (
     <>
       <header className='App-header'>
@@ -11,6 +35,7 @@ function App() {
         />
       </header>
       <div className='App'>
+        <SimpleTable data={data.articles} isFetching={data.isFetching} />
         <div className='App-link-container'>
           <a
             className='App-link'
