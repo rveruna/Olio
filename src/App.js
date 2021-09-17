984500e49e10f112f8ee285ae6436e7bbe7aaf16
Veronika Rosicova <verunar@seznam.cz>
2021-09-17 19:46:33 +0100

    moving articles to its own folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 //components
-import Articles from './components/Articles';
+import Articles from './components/Articles/Articles';
 
 // styles
 import './App.css';
