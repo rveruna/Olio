fbd127c485bda927ba54abcedfa8d6ba58c855a8
Veronika Rosicova <verunar@seznam.cz>
2021-09-22 20:36:41 +0100

    adding app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders app with welcome heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  screen.getByText('Welcome to Olio', { exact: false });
 });
