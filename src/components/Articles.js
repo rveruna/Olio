1a61a1e042a7ce765e8c6f916c84783b3e8f8787
Veronika Rosicova <verunar@seznam.cz>
2021-09-24 13:59:55 +0100

    adding link to the whole card and cleaning up some css

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 // styles
 import '../styles/Articles.css';
 
@@ -8,21 +9,26 @@ const Articles = ({ data, isFetching }) => {
     <div className='container'>
       {isFetching && 'Fetching articles...'}
       {data.map((article) => (
-        <div key={article.id} className='card'>
-          {article.images.map((image) => (
-            <img
-              src={image.files.small}
-              className='card-image'
-              alt={article.title}
-            />
-          ))}
-          <div className='card-content'>
-            <Link to={`/article/${article.title}`}>{article.title}</Link>
-            <div className='card-text' key={3}>
-              {article.description}
+        <Link to={`/article/${article.title}`}>
+          <div key={article.id} className='card'>
+            {article.images.map((image) => (
+              <img
+                src={image.files.medium}
+                className='card-image'
+                alt={article.title}
+              />
+            ))}
+            <div className='card-content'>
+              <div className='card-title'>{article.title}</div>
+              <div className='card-text' key={3}>
+                {article.description}
+              </div>
+              <div className='user-info'>
+                Posted by: {article.user.first_name}
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
